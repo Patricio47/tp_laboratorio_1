2e679315f0c01645eff1ac16cf8751f1bb48beee
Patricio47 <78211596+Patricio47@users.noreply.github.com>
2021-05-19 16:21:21 -0300

    Añadido carpeta TP1

---
+++ TP1/src/funciones.h
@@ -0,0 +1,22 @@
+/*
+ * funcioness.h
+ *
+ *  Created on: 12 abr. 2021
+ *      Author: Patricio
+ */
+
+#ifndef FUNCIONES_H_
+#define FUNCIONES_H_
+
+float sumar(float a, float b);
+float restar(float a, float b);
+float dividir(float a ,float b);
+float factorial(float a);
+float multiplicar(float a, float b);
+int menu(float a, float b);
+float numero1(void);
+float numero2(void);
+
+
+
+#endif /* FUNCIONES_H_ */
